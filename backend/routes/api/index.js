5b86f98b35a7036a0f742dc4277b1e516e25310b
Julia Fout <juuuulash@icloud.com>
2024-02-20 15:26:27 -0800

    Add error-handling middleware for common errors and debugging

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,9 +4,17 @@ const router = require('express').Router();
 //TEST SETUP
 
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
+// // fetch('/api/test', {
+// //     method: "POST",
+// //     headers: {
+// //       "Content-Type": "application/json",
+// //       "XSRF-TOKEN": "lCkIU3R4-wmtTSidgOd8xOoc_u4eAgXRw8wg"
+// //     },
+// //     body: JSON.stringify({ hello: 'world' })
+// // }).then(res => res.json()).then(data => console.log(data));
 
 module.exports = router;
